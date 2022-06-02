e70e97baaac2fa7e9a21334147a0a40dacf85890
Danilo Sales <danilo.pereira.sales@gmail.com>
2022-06-02 20:02:42 -0300

    atualizando testes

--- src/domain/psicologo/controllers/psicologo.controller.spec.ts
+++ src/domain/psicologo/controllers/psicologo.controller.spec.ts
@@ -1,5 +1,6 @@
 import app from '../../..';
 import supertest from "supertest";
+import { hasUncaughtExceptionCaptureCallback } from 'process';
 
 describe('No controller ao executar a funcao', () => {
 
@@ -17,6 +18,53 @@ describe('No controller ao executar a funcao', () => {
         });
 
       expect(response.status).toBe(201)
+    })
+
+    test("em caso de sucesso, deve retornar o response do request", async () => {
+      const response = await supertest(app)
+        .post('/psicologos')
+        .send({
+          nome: "abc3",
+          email: "abc2@gmail.com",
+          senha: "1234",
+          apresentacao: "bla bla bla bla",
+          cep: "14095050"
+        });
+
+        expect(response.body).toEqual(
+          expect.objectContaining({
+              createdAt: expect.any(String),
+              id: expect.any(Number),
+              nome: expect.any(String),
+              email: expect.any(String),
+              senha: expect.any(String),
+              apresentacao: expect.any(String),
+              bairro: expect.any(String),
+              updatedAt: expect.any(String),
+          })
+      ) 
+
+      expect(JSON.parse(response.text)).toMatchObject(
+          {
+              nome: "alo",
+              email: "nataliaAlo7@gmail.com", 
+          })
+
+      expect(response.body).toMatchObject(
+       {
+          nome: "alo",
+          email: "nataliaAlo7@gmail.com", 
+      })
+
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          nome: "abc3",
+          email: "abc3@gmail.com",
+          bairro: "Ribeirânia",
+          apresentacao: "bla bla bla bla",
+
+        })
+      )
     })
   })
 
